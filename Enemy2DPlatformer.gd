e1af4cf686958da2038833266aad50555f45a9b6
Thimble <lojith@proton.me>
2024-06-13 01:42:28 +0100

    Create Enemy2DPlatformer.gd

---
+++ Enemy2DPlatformer.gd
@@ -0,0 +1,49 @@
+#NOTE: actors will be able to navigate the game world just like the player, this includes climbing scenery
+# grappling and sliding down walls.
+extends actor
+
+#NOTE: I'm bored at work and just want to type something
+# I think what I want to make is a state machine that relies on a dictionary of operations
+# This object is an enemy type that will chase the player character
+# We need to figure out how to get its pathfinding to jump between platforms
+# https://youtu.be/OBBLgrhvaIo
+# https://stackoverflow.com/questions/67401939/how-can-i-make-a-rigid-body-jump-in-godot-without-givign-it-the-ability-to-fly
+
+@export var tilemap
+@export var player
+
+var grid_position : Vector2i:
+    get: return tilemap.world_to_map(position)
+var is_player_above : bool:
+    get: return grid_position.y < player.grid_position.y
+
+enum Operations {
+    NONE,
+    CHASE,
+    ATTACK,
+    IDLE,
+    FALL
+}
+
+#NOTE: packets may need to be defined in _ready()
+packets = {
+    Operations.CHASE : _coroutine_chase,
+    Operations.ATTACK : _coroutine_attack,
+    Operations.IDLE : _coroutine_idle
+}
+
+var current_operation = Operations.NONE
+
+func _process(delta):
+    packets[current_operation].call(delta)
+    pass
+
+#NOTE: Apparently you can use `test_motion` to detect whether a motion (such as a jump) will result in a collision
+func _coroutine_chase(delta):
+    pass
+
+func _coroutine_attack(delta):
+    pass
+
+func _coroutine_idle(delta):
+    pass
