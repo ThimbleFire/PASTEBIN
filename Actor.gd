e92be14061fccaef762eb9d6d18300a1558b602e
Thimble <lojith@proton.me>
2024-06-13 01:54:57 +0100

    Create Actor.gd

---
+++ Actor.gd
@@ -0,0 +1,29 @@
+extends RigidBody2D
+
+# DIALOGUE
+var chat_head
+var chat_name
+var chat_script
+var can_talk
+
+# NAVIGATION
+var can_jump
+var can_walk
+var can_chase
+var can_grapple
+var can_climb
+var movement_speed
+var climb_speed
+
+# MISC
+var stamina : int
+var has_stamina:
+    get: return stamina != -1
+var uses_stamina_combat
+var uses_stamina_navigating
+
+# COMBAT
+var can_take_damage
+var can_attack
+var damage
+var health
